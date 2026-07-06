c9fb3a06c16bf8f00a808dc919287ac924d7f79c
arjundj24 <sskzm5995@gmail.com>
2026-07-06 18:32:20 +0530

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://campus-connect-a3ga.onrender.com/api';
 
 const notificationList = document.getElementById('notificationList');
 const notificationSubText = document.getElementById('notificationSubText');
