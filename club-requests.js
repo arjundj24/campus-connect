97969790042c49d6b1a1d2b04d41201fbb9fba15
arjundj24 <sskzm5995@gmail.com>
2026-07-06 18:28:15 +0530

    Update club-requests.js

--- club-requests.js
+++ club-requests.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://campus-connect-a3ga.onrender.com/api';
 
 const requestSearch = document.getElementById('requestSearch');
 const requestStatusFilter = document.getElementById('requestStatusFilter');
