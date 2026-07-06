fc09e297f7bd91e49aa5bdb3971db5f319156b9d
arjundj24 <sskzm5995@gmail.com>
2026-07-06 18:33:56 +0530

    Update users.js

--- users.js
+++ users.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = 'http://localhost:5001/api';
-
+const API_BASE_URL = 'https://campus-connect-a3ga.onrender.com/api';
 const usersSearch = document.getElementById('usersSearch');
 const usersRoleFilter = document.getElementById('usersRoleFilter');
 const usersPageList = document.getElementById('usersPageList');
