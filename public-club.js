85c345a48315d006ba2981f0b7ade2a5622e1d44
arjundj24 <sskzm5995@gmail.com>
2026-07-06 18:32:46 +0530

    Update public-club.js

--- public-club.js
+++ public-club.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = 'http://localhost:5001/api';
-
+const API_BASE_URL = 'https://campus-connect-a3ga.onrender.com/api';
 const publicClubTitle = document.getElementById('publicClubTitle');
 const publicClubDescription = document.getElementById('publicClubDescription');
 const publicClubInfoBox = document.getElementById('publicClubInfoBox');
